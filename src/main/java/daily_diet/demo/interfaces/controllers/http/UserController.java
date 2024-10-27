37c457a1570148f4a9c047dd2323739fddfd6fdf
pedro henrique <pedro.henrike2812@gmail.com>
2024-10-26 22:24:42 -0300

    fix: corrigindo retorno

--- src/main/java/daily_diet/demo/interfaces/controllers/http/UserController.java
+++ src/main/java/daily_diet/demo/interfaces/controllers/http/UserController.java
@@ -3,14 +3,11 @@ package daily_diet.demo.interfaces.controllers.http;
 import daily_diet.demo.application.dto.UserDTO;
 import daily_diet.demo.application.services.UserService;
 import daily_diet.demo.domain.entities.User;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController("/user")
@@ -30,10 +27,5 @@ public class UserController {
         User user = userService.findById(id);
 
         return ResponseEntity.status(HttpStatus.OK).body(user);
-    }
-
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<String> handleConflict(ResponseStatusException ex) {
-        return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
     }
 }
